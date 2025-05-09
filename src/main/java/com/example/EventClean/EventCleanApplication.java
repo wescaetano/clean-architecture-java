36f0f9cc614e3667a520b06bd1c3a1e9d0ddd625
wescaetano <weslleycaetanosoares@gmail.com>
2025-05-09 15:49:52 -0300

    fix: Misspeling of local on sql db, drop local entity

--- src/main/java/com/example/EventClean/EventCleanApplication.java
+++ src/main/java/com/example/EventClean/EventCleanApplication.java
@@ -3,14 +3,12 @@ package com.example.EventClean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.concurrent.Executor;
-
 @SpringBootApplication
 public class EventCleanApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(EventCleanApplication.class, args);
+		SpringApplication.run(EventCleanApplication.class, args);git
 	}
 
 }
